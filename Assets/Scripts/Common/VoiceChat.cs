b7a8680f605f9a44e72cb43014a494701325d746
池田晶 <akira.0225@hotmail.com>
2020-07-17 10:56:12 +0900

    [add] とりあえずPhoton導入、未テスト

---
+++ Assets/Scripts/Common/VoiceChat.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceChat : MonoBehaviour
+{
+    // Photonプレハブのパス
+    private string _path = "PlayerVoice";
+
+    void Start ()
+    {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+    }
+
+    void OnJoinedLobby() {
+        RoomOptions options = new RoomOptions() {
+            isVisible = false,
+            maxPlayers = 4
+        };
+        PhotonNetwork.JoinOrCreateRoom("room",options,TypedLobby.Default);
+    }
+
+    void OnJoinedRoom() {
+        PhotonNetwork.Instantiate(
+            _path,
+            Vector3.zero,
+            Quaternion.identity,
+            0
+        );
+    }
+}
