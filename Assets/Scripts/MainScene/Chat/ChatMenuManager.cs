7aa21c8f1f9ec7ad8077ecb56d4a5c9558650a6b
kamigoe <akira.0225@hotmail.com>
2020-08-27 18:10:28 +0900

    [update] ChatMenuの仮実装

---
+++ Assets/Scripts/MainScene/Chat/ChatMenuManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatMenuManager : MonoBehaviour
+{
+    public static ChatMenuManager Instance;
+
+    [SerializeField] private InputField _inputFIeld = default;
+    [SerializeField] private Button _sendButton = default;
+    [SerializeField] private Button _contentButton = default;
+    [SerializeField] private RectTransform _messageParent = default;
+
+    [SerializeField] private GameObject _messagePrefab = default;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        _sendButton.onClick.AddListener(() =>
+        {
+            var message = Instantiate(_messagePrefab, _messageParent, true);
+            message.GetComponent<Message>().Initialize(null, "01/23\n04:56",_inputFIeld.text);
+        });
+        
+        _contentButton.onClick.AddListener(() =>
+        {
+            
+        });
+    }
+
+    private void Update()
+    {
+        _sendButton.interactable = !string.IsNullOrEmpty(_inputFIeld.text);
+    }
+}
---
+++ Assets/Scripts/MainScene/Chat/Message.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Message : MonoBehaviour
+{
+    private const float BaseSizeHeight = 170f;
+    
+    [SerializeField] private Image _userIcon = default;
+    [SerializeField] private Text _timeText = default;
+    [SerializeField] private Text _text = default;
+    [SerializeField] private Image _image = default;
+
+    private RectTransform _myRectTransform = default;
+
+    public void Initialize(Sprite iconImage, string time, string message)
+    {
+        _myRectTransform = GetComponent<RectTransform>();
+        _userIcon.sprite = iconImage ? iconImage : _userIcon.sprite;
+        _timeText.text = time;
+        _text.text = message;
+        Destroy(_image.transform.parent.gameObject);
+        Redraw();
+    }
+
+    public void Initialize(Sprite iconImage, string time, Sprite receiveImage)
+    {
+        _userIcon.sprite = iconImage ? iconImage : _userIcon.sprite;
+        _timeText.text = time;
+        _image.sprite = receiveImage;
+        Destroy(_text.gameObject);
+        Redraw();
+    }
+
+    private void Redraw()
+    {
+        if (_text != null)
+        {
+            var textRect = _text.GetComponent<RectTransform>();
+            var textHeight = textRect.sizeDelta.y - textRect.localPosition.y + 10;
+
+            _myRectTransform.sizeDelta = new Vector2(_myRectTransform.sizeDelta.x,
+                textHeight > BaseSizeHeight ? textHeight : BaseSizeHeight);
+        }
+        else if (_image != null)
+        {
+            _image.SetNativeSize();
+            var imageRect = _image.GetComponent<RectTransform>();
+            var size = imageRect.sizeDelta;
+            var ratio = 240f / size.x;
+
+            imageRect.sizeDelta = size * ratio;
+        }
+    }
+}
