c97b48ea864d389ba2d92a839da04a3fddf6dd64
kamigoe <akira.0225@hotmail.com>
2020-09-21 02:25:22 +0900

    [add] Photon Voiceによる仮通話

---
+++ Assets/Scripts/Common/VoiceChat.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class VoiceChat : MonoBehaviour
+{
+    // Photonプレハブのパス
+    private string _prefabPath = "PlayerVoice";
+
+    void Start ()
+    {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+    }
+
+    void OnJoinedLobby() {
+        RoomOptions options = new RoomOptions() {
+            isVisible = false
+        };
+        PhotonNetwork.JoinOrCreateRoom("room", options, TypedLobby.Default);
+    }
+
+    void OnJoinedRoom() {
+        PhotonNetwork.Instantiate(
+            _prefabPath,
+            Vector3.zero,
+            Quaternion.identity,
+            0
+        );
+    }
+}
